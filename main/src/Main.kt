b84d6f878da4f8058eb6f72b280c2f0c681598f2
jbotonakis <jbotonakis@carroll.edu>
2025-01-10 14:30:16 -0700

    Begin skeleton work: Created space for Calculator Display.

--- main/src/Main.kt
+++ main/src/Main.kt
@@ -1,19 +1,33 @@
+import java.awt.BorderLayout
+import java.awt.Color
 import javax.swing.*
 import java.awt.Dimension
 
 fun main() {
-    val frame = JFrame("Hello, World!")
-    frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-    frame.size = Dimension(500, 500)
+    val main_Jframe = JFrame("Hello, World!")
+    main_Jframe.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+    main_Jframe.size = Dimension(500, 500)
 
     val label = JLabel("Hello, World!")
     label.verticalAlignment = SwingConstants.CENTER
     label.horizontalAlignment = SwingConstants.CENTER
-    frame.add(label)
+    main_Jframe.add(label)
+
+    //Calculator Display
+    val calcDisplay = JPanel()
+    calcDisplay.background = Color.CYAN
+    calcDisplay.preferredSize = Dimension(450, main_Jframe.height/4)
+    //This doesn't work. Why? No px border, it's stuck to both left and right sides
+    //TODO: Fix the way this is rendered to give it a border
+    calcDisplay.border = BorderFactory.createEmptyBorder(150, 150, 150, 150)
+    main_Jframe.add(calcDisplay, BorderLayout.NORTH)
+
+
 
 
     //Initializes the frame in the center of the screen
-    frame.setLocationRelativeTo(null)
+    main_Jframe.setLocationRelativeTo(null)
 
-    frame.isVisible = true
+    main_Jframe.isVisible = true
+    main_Jframe.isResizable = false
 }
