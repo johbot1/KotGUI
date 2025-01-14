05bf557b7b56b1f4f579509160b66db32bf24014
John <jbotonakis@carroll.edu>
2025-01-14 10:42:19 -0700

    Turns out it was a problem. Switching gears!

--- main/src/Constructors.kt
+++ main/src/Constructors.kt
@@ -8,102 +8,57 @@ import javax.swing.*
 
 
 //PanelConstructor
-//Creates panels
-//TODO: Change this in such a way, it returns the panels created for ease of access
-class PanelConstructor(val panelName: String){
-    val panel = JPanel().also{
-        it.name = panelName
-        println("Created panel: $panelName")
-    }
-    val buttons: MutableMap<String, JButton> = mutableMapOf()
 
-    init{
-        when(panelName){
-            "display" -> {
-                val displayWidth = 250
-                val displayHeight = 125
-                panel.name = "calcDisplay"
-                panel.background = Color.CYAN
-                panel.preferredSize = Dimension(displayWidth, displayHeight)
-                panel.border = BorderFactory.createLineBorder(Color.BLACK,3,false)
-            }
-            "buttons" -> {
-                val numberLabels = listOf(0,1,2,3,4,5,6,7,8,9)
-                val buttonWidth = 80
-                val buttonHeight = 70
-                panel.name = "buttonDisplay"
-                panel.background = Color.GREEN
-                panel.border = BorderFactory.createLineBorder(Color.RED)
-                for (label in numberLabels){
-                    val button = createNumberButtons(label)
-                    button.preferredSize = Dimension(buttonWidth,buttonHeight)
-                    button.setFont(Font("Verdana", Font.PLAIN, 25))
-                    panel.add(button)
-                }
-            }
-            "operands"->{
-                val operandsLabels = listOf("+",".","-","*","/","=","clr")
-                val operationsPanel = JPanel()
-                val buttonWidth =25
-                val buttonHeight = 25
-                panel.name = "operandsDisplay"
-                operationsPanel.border = BorderFactory.createLoweredBevelBorder()
-                operationsPanel.background = Color.BLACK
-                operationsPanel.preferredSize = Dimension(250,250)
-                operationsPanel.border = BorderFactory.createLineBorder(Color.BLACK,3,false)
-                for (label in operandsLabels){
-                    val button = createOperationButtons(label)
-                    button.preferredSize = Dimension(buttonWidth,buttonHeight)
-                    button.setFont(Font("Verdana", Font.PLAIN, 25))
-                    panel.add(button)
-                }
-            }
-        }
 
-    }
+//    //createNumberButtons
+//    //Creates buttons for numbers, and returns them with
+//    //their name being "btn_number"
+//    private fun createNumberButtons(number: Int): JButton {
+//        val buttonName = "btn_$number"
+//        return JButton(number.toString()).also{
+//            it.name = buttonName
+//            //TODO: Add functionality to these buttons aside from just printing name
+//            it.addActionListener {
+//                println("${getButton(buttonName)} clicked!")
+//            }
+////            println("Created Button: ${it.name}")//Debugging tool
+//        }
+//    }
+//
+//    //createOperationButtons
+//    //Creates buttons for operations, and returns them with
+//    //their name being "btn_operation"
+//    private fun createOperationButtons(operation: String): JButton {
+//        val buttonName = "btn_$operation"
+//        val button = JButton(operation.toString()).also{
+//            it.name = buttonName
+//            //TODO: Add functionality to these buttons
+//            it.addActionListener {
+//                println("$buttonName clicked!")
+//            }
+////            println("Created Button: ${it.name}")//Debugging tool
+//        }
+//            buttons[buttonName] = button
+//        return button
+//    }
 
-    //createNumberButtons
-    //Creates buttons for numbers, and returns them with
-    //their name being "btn_number"
-    private fun createNumberButtons(number: Int): JButton {
-        val buttonName = "btn_$number"
-        return JButton(number.toString()).also{
-            it.name = buttonName
-            //TODO: Add functionality to these buttons aside from just printing name
-            it.addActionListener {
-                println("${buttonName} clicked!")
-            }
-//            println("Created Button: ${it.name}")//Debugging tool
-        }
-    }
 
-    //createOperationButtons
-    //Creates buttons for operations, and returns them with
-    //their name being "btn_operation"
-    private fun createOperationButtons(operation: String): JButton {
-        val buttonName = "btn_$operation"
-        val button = JButton(operation.toString()).also{
-            it.name = buttonName
-            //TODO: Add functionality to these buttons
-            it.addActionListener {
-                println("$buttonName clicked!")
-            }
-//            println("Created Button: ${it.name}")//Debugging tool
-        }
-            buttons[buttonName] = button
-        return button
-    }
+//val numberLabels = listOf(0,1,2,3,4,5,6,7,8,9)
+//                val buttonWidth = 80
+//                val buttonHeight = 70
+//                for (label in numberLabels){
+//                    val button = createNumberButtons(label)
+//                    button.preferredSize = Dimension(buttonWidth,buttonHeight)
+//                    button.setFont(Font("Verdana", Font.PLAIN, 25))
+//                    panel.add(button)
+//                }
 
-    //getButton
-    //Returns the button object
-    fun getButton(buttonName: String): JButton? {
-         return buttons[buttonName]
-    }
-
-    //add
-    //Adds label object to a previously created panel
-    //TODO: Finish this lol
-    fun add(label: JLabel, name: String) {
-
-    }
-}
+//val operandsLabels = listOf("+",".","-","*","/","=","clr")
+//val buttonWidth =25
+//val buttonHeight = 25
+//for (label in operandsLabels){
+//    val button = createOperationButtons(label)
+//    button.preferredSize = Dimension(buttonWidth,buttonHeight)
+//    button.setFont(Font("Verdana", Font.PLAIN, 25))
+//    panel.add(button)
+//}
--- main/src/Main.kt
+++ main/src/Main.kt
@@ -7,34 +7,46 @@ import javax.swing.*
 
 
 fun main() {
+    //mainJframe setup
     val mainJframe = JFrame("Calculator")
     mainJframe.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
     mainJframe.size = Dimension(500, 500)
     mainJframe.setLocationRelativeTo(null)
 
+    val borderThickness = 2
+
     //Calculator Display Panel
-    val calcDisplay = PanelConstructor("display")
-    mainJframe.add(calcDisplay.panel, BorderLayout.NORTH)
-
-
+    val calcDisplay = JPanel()
+    val displayWidth = 250
+    val displayHeight = 125
+    calcDisplay.name = "calcDisplay"
+    calcDisplay.background = Color.CYAN
+    calcDisplay.preferredSize = Dimension(displayWidth, displayHeight)
+    calcDisplay.border = BorderFactory.createLineBorder(Color.DARK_GRAY,borderThickness,false)
+    mainJframe.add(calcDisplay, BorderLayout.NORTH)
 
     //Calculator Display Label
     val calcLabel = JLabel("450,321.06")//Test number to gauge size
     calcLabel.setFont(Font("Verdana", Font.PLAIN, 65))
     calcDisplay.add(calcLabel, "display")
-    //FlowLayout test
-    val flowLayout = FlowLayout(FlowLayout.RIGHT)
+
 
 
     //Button Panel
     //Using the PanelConstructor, create a buttons panel
-    val buttonPanel = PanelConstructor("buttons")
-    mainJframe.add(buttonPanel.panel)
+    val buttonPanel = JPanel()
+    buttonPanel.background = Color.GREEN
+    buttonPanel.border = BorderFactory.createLineBorder(Color.DARK_GRAY,borderThickness,false)
+    mainJframe.add(buttonPanel)
 
     //Operations Panel
     //Using the PanelConstructor, create an operations panel
-    val operationsPanel = PanelConstructor("operands")
-    mainJframe.add(operationsPanel.panel, BorderLayout.EAST)
+    val operationsPanel = JPanel()
+    operationsPanel.border = BorderFactory.createLoweredBevelBorder()
+    operationsPanel.background = Color.LIGHT_GRAY
+    operationsPanel.preferredSize = Dimension(250,250)
+    operationsPanel.border = BorderFactory.createLineBorder(Color.DARK_GRAY,borderThickness,false)
+    mainJframe.add(operationsPanel, BorderLayout.EAST)
 
 
     //MainJFrame attributes
